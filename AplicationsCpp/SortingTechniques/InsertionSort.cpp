99b699dbed420621d04eaa76953ea272d0824d4a
Juan Burgos <burgos.135@gmail.com>
2024-03-07 11:26:16 -0600

    Selection sort and Insertion sort added

---
+++ AplicationsCpp/SortingTechniques/InsertionSort.cpp
@@ -0,0 +1,66 @@
+#include <stdio.h>
+
+void swap(int* x, int* y)
+{
+	int temp = *x;
+	*x = *y;
+	*y = temp;
+}
+
+void bubbleSort(int A[], int n)
+{
+	int i, j, isSorted = 0;
+
+	for (i = 0; i < n - 1; i++)
+	{
+		isSorted = 0;
+		for (j = 0; j < n - 1; j++)
+		{
+			if (A[j] > A[j + 1])
+			{
+				swap(&A[j], &A[j + 1]);
+				isSorted = 1;
+			}
+		}
+
+		if (isSorted == 0)
+			break;
+	}
+}
+
+void insertionSort(int A[], int n)
+{
+	int i, j, x;
+	
+	for (i = 1; i < n; i++)
+	{
+		j = i - 1;
+		x = A[i];
+
+		while (j > -1 && A[j] > x)
+		{
+			A[j + 1] = A[j];
+			j--;
+		}
+		A[j + 1] = x;
+
+	}
+}
+
+
+int main()
+{
+	int A[] = { 3,7,9,10,6,5,12,4,11,2 };
+	int n = 10;
+
+	insertionSort(A, 10);
+	
+	//displaying elements
+	for (int i = 0; i < 10; i++)
+	{
+		printf("%d ", A[i]);
+	}
+	printf("\n");
+
+	return 0;
+}
